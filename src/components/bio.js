3e0023de022de7897fb109f6a4e6aee0a84a184c
Taro Morita <morita0079@gmail.com>
2023-04-06 03:45:36 +0900

    githubのボタン追加した

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,9 +5,10 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import { GithubButton } from './githubButton'
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -28,29 +29,33 @@ const Bio = () => {
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
 
   return (
-    <div className="bio bg-blue-50 p-10 rounded-lg">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong>
-          {` `}
-          <p>
-          {author?.summary || null}
-          </p>
-        </p>
-      )}
+    <div>
+      <div className="bio bg-blue-50 p-10 rounded-lg">
+        <div className="w-full flex">
+          <StaticImage
+            className="bio-avatar"
+            layout="fixed"
+            formats={['AUTO', 'WEBP', 'AVIF']}
+            src="../images/profile-pic.png"
+            width={50}
+            height={50}
+            quality={95}
+            alt="Profile picture"
+          />
+          {author?.name && (
+            <p>
+              Written by <strong>{author.name}</strong>
+              {` `}
+              <p>{author?.summary || null}</p>
+            </p>
+          )}
+        </div>
+        <div>
+          <GithubButton />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/githubButton.tsx
@@ -0,0 +1,18 @@
+import { StaticImage } from 'gatsby-plugin-image'
+import React from 'react'
+
+export const GithubButton = () => {
+  return (
+    <a href="https://github.com/taro0079">
+      <StaticImage
+        layout="fixed"
+        width={30}
+        height={30}
+        quality={100}
+        src="../../static/github-mark.png"
+        alt="github"
+        className="hover:scale-125 ease-in-out duration-300"
+      />
+    </a>
+  )
+}
